4020859401f325f681c0f2a5e91caf31b908c20b
rakshit0111 <rakshitsingh333@gmail.com>
2024-09-14 19:51:32 +0530

    utils and popup

--- background.js
+++ background.js
@@ -1,13 +1,18 @@
-chrome.tabs.onUpdated.addListener((tabId,tab) => {
-  if(tab.url && tab.url.includes("youtube.com/watch"))
-  {
-    const queryParameters = tab.url.split("?")[1];
-    const urlParameters = new URLSearchParams(queryParameters);
-    
+chrome.tabs.onUpdated.addListener((tabId, tab) => {
+  if (tab.url && tab.url.includes("youtube.com/watch")) {
+      try {
+          const queryParameters = tab.url.split("?")[1];
+          const urlParameters = new URLSearchParams(queryParameters);
+          const videoId = urlParameters.get("v");
 
-    chrome.tabs.sendMessage(tabId,{
-      type : "NEW",
-      videoId : urlParameters.get("v")
-    });
+          if (videoId) {
+              chrome.tabs.sendMessage(tabId, {
+                  type: "NEW",
+                  videoId: videoId
+              });
+          }
+      } catch (error) {
+          console.error("Error parsing YouTube URL:", error);
+      }
   }
-})
+});
--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
-// adding a new bookmark row to the popup
+import { getActiveTabURL } from "./utils.js";
+
 const addNewBookmark = () => {};
 
 const viewBookmarks = () => {};
@@ -9,4 +10,23 @@ const onDelete = e => {};
 
 const setBookmarkAttributes =  () => {};
 
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", async () => {
+    const activeTab = await getActiveTabURL();
+    const queryParameters = activeTab.url.split("?")[1];
+    const urlParameters = URLSearchParams(queryParameters);
+    const currentVideo = urlParameters.get("v");
+
+    if(activeTab.url.includes("youtube.com/watch") && currentVideo)
+    {
+        chrome.storage.sync.get([currentVideo],(data) => {
+            const currentVideoBookmarks = data[currentVideo] ? JSON.parse(data[currentVideo]) : [];
+
+            // viewBookmarks function 
+        })     
+    }
+    else
+    {
+        const container = document.getElementsByClassName("container")[0];
+        container.innerHTML = '<div class = "title">This is not a YouTube video page</div>'
+    }
+});
